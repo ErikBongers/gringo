b5dc1db64df4d67822a616dd50f717df9671a342
Erik Bongers <mail@erikbongers.net>
2026-06-08 12:26:54 +0200

    Budgets updated from cloud!

--- excelScript/UpdateBudgetsFromGringo.ts
+++ excelScript/UpdateBudgetsFromGringo.ts
@@ -31,8 +31,8 @@ async function main(workbook: ExcelScript.Workbook) {
     statusField.getFormat().getFont().setColor("000000");
 
     let FolderName: string = "gringo/expenses";
-    let workbookName = workbook.getName()
-    let activeWorksheet = workbook.getActiveWorksheet()
+    let workbookName = workbook.getName();
+    let activeWorksheet = workbook.getActiveWorksheet();
     let worksheetName = activeWorksheet.getName();
     let fileName = `${FolderName}/${academie.replaceAll(" ", "_")}_${year}_expenses.json`;
     console.log(`Reading budget file: ${fileName}...`);
@@ -42,10 +42,36 @@ async function main(workbook: ExcelScript.Workbook) {
     let fullRange = activeWorksheet.getUsedRange();
     statusField.setValue("Updaten van budgetten...");
     statusField.getFormat().getFill().setColor("AAFFAA");
+    updateBudgets(workbook, cloudBudgets);
     await workbook.getActiveWorksheet().calculate(true);
     statusField.setValue("");
     statusField.getFormat().getFill().setColor("FFFFFF");
     statusField.getFormat().getFont().setColor("000000");
+}
+
+function updateBudgets(workbook: ExcelScript.Workbook, cloudBudgets: CloudBudgets) {
+    let range = workbook.getActiveWorksheet().getUsedRange();
+    //find text "Budgetpositie"
+    let budgetColumnPos = range.find("Budgetpositie", {completeMatch: true}) as ExcelScript.Range | undefined;
+    if(!budgetColumnPos) {
+        setError(workbook, `Kolomhoofding "Budgetpositie" niet gevonden.`);
+        return;
+    }
+    let brutoColumnPos = range.find("Besteed Ariba", {completeMatch: true, }) as ExcelScript.Range | undefined;
+    if(!brutoColumnPos) {
+        setError(workbook, `Kolomhoofding "Besteed Ariba" niet gevonden.`);
+        return;
+    }
+    let budgetColumn = budgetColumnPos.getColumn(0);
+    for(let budgetLine of cloudBudgets.perBudget) {
+        let budgetPos = budgetColumn.find(budgetLine.budget, {completeMatch: true}) as ExcelScript.Range | undefined;
+        if(!budgetPos) {
+            setError(workbook, `Budget ${budgetLine.budget} niet gevonden.`); //todo: create and addError() func.
+            continue;
+        }
+        let budgetCell = range.getCell(budgetPos.getRowIndex(), brutoColumnPos.getColumnIndex());
+        budgetCell.setValue(budgetLine.total);
+    }
 }
 
 function _setMessage(workbook: ExcelScript.Workbook, msg: string, type: MessageType) {
