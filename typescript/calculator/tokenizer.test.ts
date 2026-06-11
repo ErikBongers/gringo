b690a80eafa8013eb48d9deea7eaad6157be4e75
Erik Bongers <mail@erikbongers.net>
2026-06-11 12:14:18 +0200

    test

--- typescript/calculator/tokenizer.test.ts
+++ typescript/calculator/tokenizer.test.ts
@@ -4,8 +4,14 @@ import {Cursor} from "./cursor.ts";
 
 describe('Testing tokenizer', () => {
     test('test cursor', () => {
-        let cursor = new Cursor("12.34");
+        let cursor = new Cursor("1.2");
         let c = cursor.next();
         assert.equal(c, "1");
+        c = cursor.next();
+        assert.equal(c, ".");
+        c = cursor.next();
+        assert.equal(c, "2");
+        c = cursor.next();
+        assert.equal(c, "");
     });
 });
